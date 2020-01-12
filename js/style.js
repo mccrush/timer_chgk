4b2e99074f3fb2343682064fe2c5b3a758ca02b5
mccrush <mccrush2027@gmail.com>
2020-01-12 18:55:34 +0700

    Выносит стили в отдельный файл

---
+++ js/style.js
@@ -0,0 +1,9 @@
+export default style = {
+  buttonNormal: 'width: 100%; height: 40px; padding: 4px; text-align: center; outline: none; cursor: pointer; background: #4f7f5b; color: #fff; margin-top: 20px; -webkit-box-shadow: 0 .125rem .25rem rgba(0,0,0,.075) !important;  font-size:16px; border-radius: 4px; border-style: hidden;',
+  buttonActive: 'width: 100%; height: 40px; padding: 4px; text-align: center; outline: none; cursor: pointer; background: #eee; color: #373A3C; margin-top: 20px; -webkit-box-shadow: 0 .125rem .25rem rgba(0,0,0,.075) !important;  font-size:16px; border-radius: 4px; border-style: hidden;',
+  buttonRed: 'width: 100%; height: 40px; padding: 4px; text-align: center; outline: none; cursor: pointer; background: #AA2222; color: #fff; margin-top: 20px; -webkit-box-shadow: 0 .125rem .25rem rgba(0,0,0,.075) !important;  font-size:16px; border-radius: 4px; border-style: hidden;',
+  checkboxTime: 'float: left; margin-left: 20px;',
+  checkboxSound: 'float: left',
+  divMain: 'position: fixed; width: 150px; top: 400px; left: 200px; background: rgba(255, 255, 255, 0.65); text-align: center; z-index: 9999; padding: 20px 10px 10px 10px;',
+  divTime: 'text-align: center; font-size: 40px; color: #818A91;'
+}
